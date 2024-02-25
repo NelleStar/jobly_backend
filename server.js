adb987e10367c868455405cf31be24dbf384f4b4
Janelle <janelle.k.houston@gmail.com>
2024-02-25 17:36:59 -0500

    take 10

--- server.js
+++ server.js
@@ -2,7 +2,13 @@
 
 const app = require("./app");
 const { PORT } = require("./config");
+const cors = require("cors")
 
+app.use(
+  cors({
+    origin: "https://nellestar.github.io",
+  })
+);
 app.listen(PORT, function () {
   console.log(`Started on http://localhost:${PORT}`);
 });
